ea2f06cfdbbe705cd6c7074930caf76b6e64fc36
Kumar Rishabh <kr.rishabh72@gmail.com>
2021-06-11 16:22:22 +0530

    First commit

---
+++ livechartsFirstTesting/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace livechartsFirstTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "value 1",
+                    Values = new ChartValues<double>{1,2,3,4,5,6},
+                }
+            };
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "value 2",
+                Values = new ChartValues<double> { 2, 4, 5, 8, 6, 9 },
+            });
+
+            BarLabels = new string[] { "value1", "value2", "value1", "value2", "value1" };
+            Formatter = value => value.ToString("N");
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; }
+        public string[] BarLabels { get; }
+        public Func<double, string> Formatter { get; set; }
+    }
+}
